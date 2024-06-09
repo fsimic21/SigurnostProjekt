df0ef0924e0c57440b725c2fcc32fc6c50354318
Frano Šimić <126497343+fsimic21@users.noreply.github.com>
2024-06-09 13:06:00 +0200

    Inšalah da je popravljeno

--- SPM/SPM/src/main/java/com/SIS/SPM/config/FirebaseInitializer.java
+++ SPM/SPM/src/main/java/com/SIS/SPM/config/FirebaseInitializer.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class FirebaseInitializer {
@@ -15,15 +19,26 @@ public class FirebaseInitializer {
     @PostConstruct
     public void initialize() throws IOException {
         FileInputStream serviceAccount = null;
-        try{
-            serviceAccount = new FileInputStream("./serviceAccountKey.json");
+        try {
+            Path path = Paths.get("SPM/serviceAccountKey.json");
+            System.out.println("Path to serviceAccountKey.json: " + path.toAbsolutePath().toString());
+
+            if (!Files.exists(path)) {
+                throw new FileNotFoundException("serviceAccountKey.json not found at " + path.toAbsolutePath().toString());
+            }
+
+            serviceAccount = new FileInputStream(path.toString());
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
             FirebaseApp.initializeApp(options);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (serviceAccount != null) {
+                serviceAccount.close();
+            }
         }
     }
 }
